26a2f6db8dbae0b6739b8d60e6644237fe563a30
wupeiyin <1136970917@qq.com>
2017-09-11 16:00:30 +0800

    第三次提交，怎加了Bean类 2017-9-11 16:00:23

---
+++ app/src/main/java/com/example/dai01git/Bean.java
@@ -0,0 +1,13 @@
+package com.example.dai01git;
+
+import android.util.Log;
+
+/**
+ * Created by 勿忘初心 on 2017/9/11.
+ */
+
+public class Bean {
+    public void data(){
+        Log.e("TAG", "data: 第3次修改" );
+    }
+}
